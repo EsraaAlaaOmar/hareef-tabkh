2168fd3ab50dee7ff985321d1d5e7780dc126f9c
EsraaAlaaOmar <esraa.safa55@gmail.com>
2023-06-07 16:54:42 +0300

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/reusable/Navbar'
+const index = () => {
+  return (
+      <>
+       <Navbar />
+      </>
+  )
+}
+
+export default index
