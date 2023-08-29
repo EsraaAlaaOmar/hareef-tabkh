0eace08a77b423bd2a40242980ad1266697ee290
EsraaAlaaOmar <esraa.safa55@gmail.com>
2023-08-29 14:48:54 +0300

    img

--- components/reusable/Loader.tsx
+++ components/reusable/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 const Loader = () => {
   return (
-    <Image  width={100}height={100} alt='loading' src="/images/rolling.gif" style={{  margin:" 20px auto"}} /> 
+    <img  alt='loading' src="/images/Rolling.gif" style={{  margin:" 20px auto"}} /> 
   )
 }
 
