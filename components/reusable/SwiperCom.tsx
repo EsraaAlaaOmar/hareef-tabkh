7c3124ea25025fe2d7dd11139e247f2c9715935d
EsraaAlaaOmar <esraa.safa55@gmail.com>
2023-10-04 18:46:38 +0300

    push

--- components/reusable/SwiperCom.tsx
+++ components/reusable/SwiperCom.tsx
@@ -7,7 +7,7 @@ const SwiperCom = () => {
     <Carousel autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false} showStatus={false} autoFocus={true}>
     <div>
        <img src="/images/slider1.png" />
-       <p className="legend" >
+       <p className="legend first-slide" >
           عندك موهبة ونفسك تشاركها مع الناس؟
           <br/>
           فرصتك جت يلا شاركنا مستنى ايه!
@@ -20,7 +20,7 @@ const SwiperCom = () => {
     </div>
     <div>
         <img src="/images/slider2.png" />
-        <p className="legend" >
+        <p className="legend second-slide" >
         في ايديك دلوقتي تطلع للنور حمِل فيديوهات لموهبتك وشاركها مع الناس!
         <br/> 
         <button className='add-vid'>اضافة فيديو </button>
@@ -29,7 +29,7 @@ const SwiperCom = () => {
     </div>
     <div>
         <img src="/images/slider3.png" />
-        <p className="legend" >
+        <p className="legend  third-slide" >
         اطلب #37# دلوقتي أو زور اللينك للاشتراك في خدمة لو موهوب. مع اورنج هتقدر تشارك فيديوهاتك مع صحابك و أقاربك .
         <br/> 
         <button className='add-vid'>اضافة فيديو </button>
