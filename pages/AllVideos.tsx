289d62f2819984b041adf0c1bbb12a482d944724
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-07-02 11:42:31 +0300

    typescript error

--- pages/AllVideos.tsx
+++ pages/AllVideos.tsx
@@ -17,7 +17,7 @@ interface VideoData {
   // title: string;
   // url: string;
   // Add other properties as needed
-  DateIn: Date;
+  DateIn: string;
   Deleted: Boolean;
   Description: string;
   NViews: number;
