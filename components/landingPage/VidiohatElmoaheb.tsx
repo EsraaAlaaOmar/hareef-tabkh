e6412c19ab0a8f3d2d9b9a30134f245580b163bf
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-07-03 13:38:18 +0300

    remove updates

--- components/landingPage/VidiohatElmoaheb.tsx
+++ components/landingPage/VidiohatElmoaheb.tsx
@@ -13,7 +13,7 @@ interface VideoData {
   // title: string;
   // url: string;
   // Add other properties as needed
-  DateIn: string;
+ DateIn: string;
   Deleted: Boolean;
   NShares: number;
   Description: string;
