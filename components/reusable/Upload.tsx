7cd8152043fc20a0e062980fb23fff577ee53934
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-07-18 08:40:01 +0300

    update url

--- components/reusable/Upload.tsx
+++ components/reusable/Upload.tsx
@@ -85,8 +85,8 @@ interface UploadProps {
      
   console.log(formData)
       try {
-        const response = await axios.post("`https://vodafone.alerting.services/LawMawhobApis/LawMawhobApis/Talents/Addvideo", reqdata,{ headers: {
-          "Api_Key": "elinxfthr62023",
+        const response = await axios.post("https://vodafone.alerting.services/LawMawhobApis/Talents/Addvideo", reqdata,{ headers: {
+         
           'content-type': 'text/json'
         }});
       
