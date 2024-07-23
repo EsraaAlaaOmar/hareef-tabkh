d26e32dc7ef762c598b633fe9544bb917fc1f116
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-07-23 14:59:00 +0300

    terms in footer

--- components/reusable/Footer.tsx
+++ components/reusable/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
       <div className='links'>
      <Link  href='/about'><span className='link'>عن لو موهوب</span></Link> 
      {/* <Link  href='/terms'><span className='link'>شروط المسابقة</span></Link>  */}
-     {/* <Link  href='/terms'><span className='link'>الشروط والأحكام</span></Link>  */}
+     <Link  href='/terms'><span className='link'>الشروط والأحكام</span></Link> 
      <Link  href='/privacy'><span className='link'>سياسة الخصوصية</span></Link> 
       </div>
       <div className='lastline'>
