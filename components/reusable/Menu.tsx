ed34d7e6ccb505d2f530e8d172d1c1f88017302a
EsraaAlaaOmar <esraa.safa55@gmail.com>
2025-02-24 12:57:25 +0400

    add links

--- components/reusable/Menu.tsx
+++ components/reusable/Menu.tsx
@@ -15,21 +15,21 @@ const phoneNumber =(typeof window !== 'undefined')&&localStorage.getItem("MSISDN
     <div id="divMenu" className="menu-list active" ref={ref}>
       <span className='close'><IoIosCloseCircleOutline  onClick={()=>setshowList()}/></span>
         <ul className="list-unstyled">
-            <li><a href="#" className="na">
+            <li><a href="http://www.hareeftabkh.com/signin" className="na">
                 <i className="far fa-user"></i><span>  {phoneNumber ?`مرحبا ${phoneNumber}` :"تسجيل الدخول"}</span></a>
                 </li>
-            <li><a href="index" className="active">
+            <li><a href="http://www.hareeftabkh.com/" className="active">
                 <i className="fas fa-home"></i>
              <span> الرئيسية </span></a>
              </li>
-             <li><a href="/favorites" className="na"><i className="fas fa-heart"></i> <span> المفضلة </span></a></li>
+             <li><a href="/http://www.hareeftabkh.com/index?state=signin" className="na"><i className="fas fa-heart"></i> <span> المفضلة </span></a></li>
              <li><Link href="/AllVideos"><i className="fas fa-heart"></i> <span> المسابقة </span></Link></li>
              <li><Link href="/myvideos" className="na"><i className="fas fa-heart"></i> <span> فديوهتي </span></Link></li>
-             <li><a href="categories" className="na"><i className="fas fa-concierge-bell"></i> <span> التصنيفات </span></a></li>
-             <li><a href="mostviewed" className="na"><i className="fas fa-fire"></i> <span> الأكثر مشاهدة </span></a></li>
-             <li><a href="latest" className="na"><i className="fas fa-utensils"></i> <span> أحدث الوصفات </span></a></li>
-             <li><a href="terms" className="na"><i className="fas fa-question"></i> <span> الشروط و الأحكام </span></a>
-             </li><li><a href="signout" className="na"><i className="fas fa-sign-out-alt"></i> <span>تسجيل الخروج</span></a></li> 
+             <li><a href="/http://www.hareeftabkh.com/categories" className="na"><i className="fas fa-concierge-bell"></i> <span> التصنيفات </span></a></li>
+             <li><a href="/http://www.hareeftabkh.com/mostviewed" className="na"><i className="fas fa-fire"></i> <span> الأكثر مشاهدة </span></a></li>
+             <li><a href="/http://www.hareeftabkh.com/latest" className="na"><i className="fas fa-utensils"></i> <span> أحدث الوصفات </span></a></li>
+             <li><a href="/http://www.hareeftabkh.com/terms" className="na"><i className="fas fa-question"></i> <span> الشروط و الأحكام </span></a>
+             </li><li><a href="/http://www.hareeftabkh.com/signin" className="na"><i className="fas fa-sign-out-alt"></i> <span>تسجيل الخروج</span></a></li> 
              <li><a href="unsubconfirm" className="na"><i className="fas fa-sign-out-alt"></i> <span>الغاء الاشتراك</span></a></li> 
              <ul className="list-unstyled"></ul></ul>
      
